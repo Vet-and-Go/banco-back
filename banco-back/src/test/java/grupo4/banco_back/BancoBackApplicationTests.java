3a947e8702cba3aace803b767da795e30dce742e
Alejandro M <mapi1210ceo@gmail.com>
2025-10-31 11:53:54 +0100

    Añadido tablas y casi completado

---
+++ banco-back/src/test/java/grupo4/banco_back/BancoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo4.banco_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
