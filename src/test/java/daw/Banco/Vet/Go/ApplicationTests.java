8f41979e870784a33d879c995337e728824526ed
aivan <aivanalbaranez@gmail.com>
2025-11-28 13:43:58 +0100

    First commit

---
+++ src/test/java/daw/Banco/Vet/Go/ApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.Banco.Vet.Go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
