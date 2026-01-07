bda8e272bd6c2e7f11f0e562f560da56a8c49513
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-07 13:08:20 +0100

    m

--- banco-back/src/main/java/grupo4/banco_back/service/PaymentService.java
+++ banco-back/src/main/java/grupo4/banco_back/service/PaymentService.java
@@ -1,5 +1,19 @@
 package grupo4.banco_back.service;
 
-public interface PaymentService {
+import java.util.Date;
 
+public interface PaymentService {
+  // Are login & apiToken valid?
+  Boolean validateStore(
+      String login,
+      String apiToken);
+
+  // Is the card valid?
+  Boolean validateCard(
+      String login,
+      String apiToken,
+      String cardNumber,
+      Date expirationDate,
+      String cvv,
+      String fullName);
 }
