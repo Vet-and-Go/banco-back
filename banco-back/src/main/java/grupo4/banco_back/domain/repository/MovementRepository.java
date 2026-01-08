586d3b0a87b1251a66b3afd8cc58885b2deadd0f
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-08 12:22:04 +0100

    Слава, как жезнь?

---
+++ banco-back/src/main/java/grupo4/banco_back/domain/repository/MovementRepository.java
@@ -0,0 +1,33 @@
+package grupo4.banco_back.domain.repository;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public interface MovementRepository {
+  Boolean validateStore(
+      String login,
+      String apiToken);
+
+  Boolean validateCard(
+      String login,
+      String cardNumber,
+      Date expirationDate,
+      String cvv,
+      String fullName);
+
+  Boolean validateAccountRelation(
+      String login,
+      String destinationIban);
+
+  Boolean validateAblePayment(
+      String cardNumber,
+      Double amount);
+
+  void processPayment(
+      String cardNumber,
+      String destinationIban,
+      BigDecimal amount,
+      String concept);
+
+  void findAll();
+}
