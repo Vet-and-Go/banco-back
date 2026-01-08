51a7ac16ca1df7025a18212bc2ae5ad7ff9036eb
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-08 09:50:43 +0100

    more

--- banco-back/src/main/java/grupo4/banco_back/persistence/dao/jpa/BankMovementJpaEntity.java
+++ banco-back/src/main/java/grupo4/banco_back/persistence/dao/jpa/BankMovementJpaEntity.java
@@ -1,6 +1,11 @@
 package grupo4.banco_back.persistence.dao.jpa;
 
+import grupo4.banco_back.domain.enums.*;
+import grupo4.banco_back.domain.model.CreditCard;
+
 import jakarta.persistence.*;
+import java.util.Date;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "bank_movements")
@@ -8,21 +13,31 @@ public class BankMovementJpaEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   Long id;
-  @Column(name = "iban")
-  String iban;
-  @Column(name = "balance")
-  Double balance;
-  @Column(name = "clientDni")
-  String clientDni;
+  @Column(name = "type")
+  MOVEMENT_TYPE type;
+  @Column(name = "origin")
+  MOVEMENT_ORIGIN origin;
+  @Column(name = "creditCard")
+  CreditCard creditCard;
+  @Column(name = "date")
+  Date date;
+  @Column(name = "amount")
+  BigDecimal amount;
+  @Column(name = "concept")
+  String concept;
 
   public BankMovementJpaEntity() {
   }
 
-  public BankMovementJpaEntity(Long id, String iban, Double balance, String clientDni) {
+  public BankMovementJpaEntity(Long id, MOVEMENT_TYPE type, MOVEMENT_ORIGIN origin, CreditCard creditCard, Date date,
+      BigDecimal amount, String concept) {
     this.id = id;
-    this.iban = iban;
-    this.balance = balance;
-    this.clientDni = clientDni;
+    this.type = type;
+    this.origin = origin;
+    this.creditCard = creditCard;
+    this.date = date;
+    this.amount = amount;
+    this.concept = concept;
   }
 
   public Long getId() {
@@ -33,28 +48,52 @@ public class BankMovementJpaEntity {
     this.id = id;
   }
 
-  public String getIban() {
-    return iban;
+  public MOVEMENT_TYPE getType() {
+    return type;
   }
 
-  public void setIban(String iban) {
-    this.iban = iban;
+  public void setType(MOVEMENT_TYPE type) {
+    this.type = type;
   }
 
-  public Double getBalance() {
-    return balance;
+  public MOVEMENT_ORIGIN getOrigin() {
+    return origin;
   }
 
-  public void setBalance(Double balance) {
-    this.balance = balance;
+  public void setOrigin(MOVEMENT_ORIGIN origin) {
+    this.origin = origin;
   }
 
-  public String getClientDni() {
-    return clientDni;
+  public CreditCard getCreditCard() {
+    return creditCard;
   }
 
-  public void setClientDni(String clientDni) {
-    this.clientDni = clientDni;
+  public void setCreditCard(CreditCard creditCard) {
+    this.creditCard = creditCard;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public String getConcept() {
+    return concept;
+  }
+
+  public void setConcept(String concept) {
+    this.concept = concept;
   }
 
   @Override
