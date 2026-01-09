2a099c97708fc61c22a1744d30d60641e03499e7
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-09 09:16:36 +0100

    Cumberland Gap

--- banco-back/src/main/java/grupo4/banco_back/controller/PaymentController.java
+++ banco-back/src/main/java/grupo4/banco_back/controller/PaymentController.java
@@ -4,23 +4,34 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import grupo4.banco_back.domain.dto.PaymentDto;
-import grupo4.banco_back.domain.service.cases.PaymentService;
+import grupo4.banco_back.domain.service.cases.PaymentCase;
 
 @RestController
 @RequestMapping("/api/bank")
 public class PaymentController {
 
-  private final PaymentService paymentService;
+  private final PaymentCase paymentCase;
 
-  public PaymentController(PaymentService paymentService) {
-    this.paymentService = paymentService;
+  public PaymentController(PaymentCase paymentCase) {
+    this.paymentCase = paymentCase;
   }
 
   @GetMapping("")
-  public ResponseEntity<List<PaymentDto>> getAll() {
+  public ResponseEntity<List<PaymentDto>> getAllPayments() {
+    // List<PaymentDto> payments = paymentCase.getAll();
+    // return ResponseEntity.ok(payments);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/payments")
+  public ResponseEntity<Void> processPayment(@RequestBody PaymentDto paymentDto) {
+    paymentCase.processPayment(paymentDto);
+    return ResponseEntity.ok().build();
   }
 }
--- banco-back/src/main/java/grupo4/banco_back/domain/service/cases/PaymentCase.java
+++ banco-back/src/main/java/grupo4/banco_back/domain/service/cases/PaymentCase.java
@@ -2,7 +2,7 @@ package grupo4.banco_back.domain.service.cases;
 
 import grupo4.banco_back.domain.dto.PaymentDto;
 
-public interface PaymentServiceCase {
+public interface PaymentCase {
 
   void processPayment(PaymentDto paymentDto);
 
