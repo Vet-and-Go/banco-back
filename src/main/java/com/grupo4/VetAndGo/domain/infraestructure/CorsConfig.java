e65331040e92137029d6c631d1a3678c7a7caa77
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-29 09:33:50 +0100

    New origins allowed

--- src/main/java/com/grupo4/VetAndGo/domain/infraestructure/CorsConfig.java
+++ src/main/java/com/grupo4/VetAndGo/domain/infraestructure/CorsConfig.java
@@ -10,7 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOriginPatterns("*")
+        .allowedOrigins(
+            "http://localhost:3000",
+            "http://localhost:4200",
+            "http://vetandgo-bank-front.preproducciondaw.cip.fpmislata.com",
+            "http://vetandgo-store-front.preproducciondaw.cip.fpmislata.com"
+        )
         .allowedMethods("*")
         .allowedHeaders("*")
         .allowCredentials(true)
